51c0c76e0ad00b40e43b559122f87c12784adea2
AnnSokolovaa <spe4pg@gmail.com>
2025-06-09 01:13:11 +0300

    feat: Initial

---
+++ lua/checkbox-sync.lua
@@ -0,0 +1,117 @@
+local utils = require("checkbox-sync.utils")
+
+local M = {}
+
+---@class CheckboxSync.Config
+M.confing = {
+
+	---@type string
+	todo_status = "[-]",
+
+	---@type boolean
+	insert_leave = true,
+}
+
+---Update node status under cursor. Will not trigger update of upper nodes
+---
+---@param node TSNode? Node to update. Defaults to node under cursor
+---@param down boolean? If true, will update immediate children instead of self
+---@return number?, number?
+function M.update(node, down)
+	local parser = utils.get_parser()
+	assert(parser:lang() == "markdown", "Callback should be called only inside of markdown!")
+	local cur_list_item = utils.get_list_item(node or utils.get_current())
+	if not cur_list_item then
+		return
+	end
+
+	local status, row, col = utils.get_status(cur_list_item)
+	if not status then
+		return
+	end
+
+	local _, list_match = utils.parsed_query:iter_matches(cur_list_item, 0)()
+	if not list_match then
+		return
+	end
+
+	local list = list_match[1][1]
+	local has_empty = false
+	local has_done = false
+	local has_todo = false
+	local update_children = down and (status == "[ ]" or status == "[x]")
+	for item in list:iter_children() do
+		local item_status, item_row, item_col = utils.get_status(item)
+		if vim.tbl_contains({
+			M.confing.todo_status,
+			"[ ]",
+			"[x]",
+		}, item_status) then
+			if update_children then
+				assert(item_col and item_row, "Will never fire, needed for lua ls")
+				utils.replace_status(status, item_row, item_col)
+			end
+		end
+		if item_status == "[x]" then
+			has_done = true
+		end
+		if item_status == "[ ]" then
+			has_empty = true
+		end
+		if item_status == M.confing.todo_status then
+			has_todo = true
+		end
+	end
+	if update_children then
+		return
+	end
+	local new_status = status
+	if has_empty then
+		new_status = (has_done or has_todo) and M.confing.todo_status or "[ ]"
+	elseif has_todo then
+		new_status = M.confing.todo_status
+	elseif has_done then
+		new_status = "[x]"
+	end
+	if new_status ~= status then
+		assert(col and row, "Will never fire, needed for lua ls")
+		utils.replace_status(new_status, row, col)
+		vim.cmd("silent! w")
+	end
+	return row, col
+end
+
+---Update all ancestors of checkbox under cursor
+---
+---@param node TSNode? When specified, updates ancestors of the given node instead
+function M.update_ancestors(node)
+	local cur_node = utils.get_list_item(node or utils.get_current()):parent()
+
+	if not cur_node then
+		return
+	end
+
+	local row, col = M.update(cur_node)
+	if not (row and col) then
+		return
+	end
+
+	cur_node = utils.refetch_node(row, col)
+	M.update_ancestors(cur_node)
+end
+
+---@param opts CheckboxSync.Config?
+function M.setup(opts)
+	M.confing = vim.tbl_extend("force", M.confing, opts)
+	if M.confing.insert_leave then
+		vim.api.nvim_create_autocmd("InsertLeave", {
+			pattern = "*.md",
+			callback = function()
+				M.update()
+				M.update_ancestors()
+			end,
+		})
+	end
+end
+
+return M
---
+++ lua/checkbox-sync/utils.lua
@@ -0,0 +1,87 @@
+local M = {}
+
+local ts = vim.treesitter
+
+function M.get_parser()
+	return require("nvim-treesitter.parsers").get_parser()
+end
+
+---@return TSNode
+function M.get_current()
+	local cur_node = ts.get_node()
+	assert(cur_node, "This aint a tree!")
+	return cur_node
+end
+
+---@param node TSNode
+---@return TSNode?
+function M.get_list_item(node)
+	if node:type() == "list_item" then
+		return node
+	end
+	local parent = node:parent()
+	if parent == nil then
+		return nil
+	end
+	return M.get_list_item(parent)
+end
+
+local query = [[
+(list) @list
+]]
+M.parsed_query = ts.query.parse("markdown", query)
+
+---@param list_item TSNode
+---@param todo_status string
+---@return string?, number?, number?
+function M.get_status(list_item, todo_status)
+	assert(list_item:type() == "list_item", "Item should be of type list_item")
+	local body = list_item:child(1)
+	assert(body, "At this point should not fire. Somthing is wrong with parser if it does")
+	local row, col = body:start()
+	if body:type() == "task_list_marker_checked" or body:type() == "task_list_marker_unchecked" then
+		return ts.get_node_text(body, 0), row, col
+	end
+
+	-- Kinda hacky (esp with lua and utf-8), but works.
+	-- Reason is, at this point we know that we are inside of a list item.
+	local status = ts.get_node_text(body, 0):sub(0, 3)
+	if status ~= todo_status then
+		return nil
+	end
+	return status, row, col
+end
+
+---@param new_status string
+---@param row number
+---@param col number
+---@param todo_status string
+function M.replace_status(new_status, row, col, todo_status)
+	assert(
+		vim.tbl_contains({
+			"[ ]",
+			"[x]",
+			todo_status,
+		}, new_status),
+		"Invalid status"
+	)
+	assert(row, "Row must be specified!")
+	assert(col, "Col must be specified!")
+
+	local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
+	local new_line = line:sub(0, col) .. new_status .. line:sub(col + 4, #line + 1)
+	vim.api.nvim_buf_set_lines(0, row, row + 1, false, { new_line })
+end
+
+---@param row number
+---@param col number
+---@return TSNode?
+function M.refetch_node(row, col)
+	local parser = M.get_parser()
+	parser:invalidate()
+	return ts.get_node({
+		bufnr = 0,
+		pos = { row, col },
+	})
+end
+return M
